5c61cd207836da2af70fae35a01a5f6084dfe89e
Sam Bencivengo <sambencivengo@gmail.com>
2021-11-07 21:33:50 -0500

    commit tree 20195b2ce241498b045f82d54dc6f5b09e10b083

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
 
+  attr_accessor :name, :breed, :id
+
+  def initialize(name:, breed:, id: nil)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+    SQL
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+    DB[:conn].execute(sql)
+  end
+
+  # END OF CLASS
 end
